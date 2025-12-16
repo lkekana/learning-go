3bb189b0dcd43954998767d7fc369440fbf3b294
Lesedi Kekana <theoriginalles@gmail.com>
2025-12-16 14:11:55 +0200

    group CLI functionality into functions

--- main.go
+++ main.go
@@ -8,6 +8,69 @@ import (
 	"todos"
 )
 
+func addTaskCLI(args []string, tasks *todos.Tasks) {
+	if len(args) < 3 {
+		fmt.Println("Please provide a task name.")
+		return
+	}
+	task := strings.Join(args, " ")
+	tasks.AddTask(task)
+	fmt.Println("Added task:", task)
+}
+
+func removeTaskCLI(args []string, tasks *todos.Tasks) {
+	if len(args) < 3 {
+		fmt.Println("Please provide a task ID to remove.")
+		return
+	}
+	taskID := args[2]
+	t, err := tasks.RemoveTask(taskID)
+	if err != nil {
+		fmt.Println("Error removing task:", err)
+		return
+	}
+	fmt.Println("Removed task:", t)
+}
+
+func toggleTaskCLI(args []string, tasks *todos.Tasks) {
+	if len(args) < 3 {
+		fmt.Println("Please provide a task ID to toggle.")
+		return
+	}
+	taskID := args[2]
+	t, err := tasks.GetTask(taskID)
+	if err != nil {
+		fmt.Println("Error finding task:", err)
+		return
+	}
+	t.ToggleCompleted()
+	fmt.Println("Toggled task:", t)
+}
+
+func editTaskCLI(args []string, tasks *todos.Tasks) {
+	if len(args) < 4 {
+		fmt.Println("Please provide a task ID and new name to edit.")
+		return
+	}
+	taskID := args[2]
+	newName := strings.Join(args[3:], " ")
+	t, err := tasks.EditTask(taskID, newName)
+	if err != nil {
+		fmt.Println("Error editing task:", err)
+		return
+	}
+	fmt.Println("Edited task:", t)
+}
+
+func listTasksCLI(tasks *todos.Tasks) {
+	if len(tasks.Tasks) == 0 {
+		fmt.Println("No tasks found.")
+		return
+	}
+	fmt.Println("\nTasks:")
+	fmt.Println(tasks.String())
+}
+
 func main() {
     args := os.Args
 
@@ -27,67 +90,17 @@ func main() {
 
 	switch cmd {
 		case "add":
-			if len(args) < 3 {
-				fmt.Println("Please provide a task name.")
-				return
-			}
-			task := strings.Join(args[2:], " ")
-			tasks.AddTask(task)
-			fmt.Println("Added task:", task)
+			addTaskCLI(args, &tasks)
 		case "remove":
-			if len(args) < 3 {
-				fmt.Println("Please provide a task ID to remove.")
-			}
-			taskID := args[2]
-			t, err := tasks.RemoveTask(taskID)
-			if err != nil {
-				fmt.Println("Error removing task:", err)
-				return
-			}
-			fmt.Println("Removed task:", t)
+			removeTaskCLI(args, &tasks)
 		case "toggle":
-			if len(args) < 3 {
-				fmt.Println("Please provide a task ID to toggle.")
-			}
-			taskID := args[2]
-			t, err := tasks.GetTask(taskID)
-			if err != nil {
-				fmt.Println("Error finding task:", err)
-				return
-			}
-			t.ToggleCompleted()
-			fmt.Println("Toggled task:", t)
+			toggleTaskCLI(args, &tasks)
 		case "edit":
-			if len(args) < 3 {
-				fmt.Println("Please provide a task ID to edit.")
-			}
-			taskID := args[2]
-			t, err := tasks.GetTask(taskID)
-			if err != nil {
-				fmt.Println("Error finding task:", err)
-				return
-			}
-			newName := strings.Join(args[3:], " ")
-			t, err = tasks.EditTask(t.ID, newName)
-			if err != nil {
-				fmt.Println("Error editing task:", err)
-				return
-			}
-			fmt.Println("Edited task name to:", newName)
+			editTaskCLI(args, &tasks)
 		case "list":
-			if len(tasks.Tasks) == 0 {
-				fmt.Println("No tasks found.")
-				return
-			}
-			fmt.Println("\nTasks:")
-			fmt.Println(tasks.String())
+			listTasksCLI(&tasks)
 		case "tasks":
-			if len(tasks.Tasks) == 0 {
-				fmt.Println("No tasks found.")
-				return
-			}
-			fmt.Println("\nTasks:")
-			fmt.Println(tasks.String())
+			listTasksCLI(&tasks)
 		case "serve":
 			todos.Serve()
 		default:
